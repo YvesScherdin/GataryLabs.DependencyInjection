0ee086011329dd2a0d083d4dd584424cfea73bf4
Yves Scherdin <yves.scherdin@gmx.de>
2025-07-29 22:36:08 +0200

    improve inj flgs

--- GataryLabs.DependencyInjection/InjectionFlags.cs
+++ GataryLabs.DependencyInjection/InjectionFlags.cs
@@ -1,16 +1,56 @@
 ﻿namespace GataryLabs.DependencyInjection
 {
+    /// <summary>
+    /// Flags with which the injection can be customized.
+    /// </summary>
     public enum InjectionFlags
     {
+        /// <summary>
+        /// Includes no members.
+        /// </summary>
         None = 0,
+
+        /// <summary>
+        /// Includes fields decorated with inject attribute.
+        /// </summary>
         Fields = 1,
+
+        /// <summary>
+        /// Includes properties decorated with inject attribute.
+        /// </summary>
         Properties = 2,
+
+        /// <summary>
+        /// Includes methods decorated within this type with the inject attribute (lacking those decorated in the base class)
+        /// </summary>
         Methods = 4,
+
+        /// <summary>
+        /// Includes the first constructor that is decorated with the inject attribute.
+        /// </summary>
         Constructors = 8,
 
         // composite presets
+
+        /// <summary>
+        /// Composite flags, including just fields and properties.
+        /// </summary>
         Simple = Fields | Properties,
-        Default = Fields | Properties | Constructors,
+
+        /// <summary>
+        /// Composite that lacks only methods.
+        /// </summary>
+        AllButMethods = Fields | Properties | Constructors,
+
+        /// <summary>
+        /// Composite that lacks only methods. For performance reasons, this is default.
+        /// Many methods are resolved from even a class without custom methods.
+        /// </summary>
+        Default = AllButMethods,
+
+        /// <summary>
+        /// All.
+        /// </summary>
         All = Fields | Properties | Methods | Constructors
     }
 }
